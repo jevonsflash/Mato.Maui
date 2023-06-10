4e2190eed095ca0a43deba77c8be1894db86623c
Xamarin <jevonsflash@qq.com>
2023-06-10 11:36:36 +0800

    init commit

---
+++ MatoRichTextEditor/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MatoRichTextEditor;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MatoRichTextEditor/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MatoRichTextEditor;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
